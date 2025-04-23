0587ce150150f744eea746a89fe6ab6587090596
Victor <paulovtsouza@outlook.com>
2025-04-23 17:34:13 -0300

    Adição da branch exercicio_html_js

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.formulario');
+const campoA = document.getElementById('campoA')
+const campoB = document.getElementById('campoB')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const valor1 = campoA.value
+    const valor2 = campoB.value
+    const mensagemSucess = 'O resultado dos numeros inseridos é valido'
+    const mensagemError = 'O resultado dos numeros inseridos é invalido'
+    const resultado = document.querySelector('.mensagemValidacao')
+    const errorNum = document.querySelector('.mensagemNum')
+    const validation = document.querySelector('mensagemValidacao')
+
+    if (isNaN(valor1) || isNaN(valor2)){
+        errorNum.style.display = 'block'
+    }
+    else if(Number(valor1) < Number(valor2)){
+        document.querySelector('.mensagemValidacao').style.display = 'block'
+        resultado.textContent = mensagemSucess
+        }
+        else{
+        document.querySelector('.mensagemValidacao').style.backgroundColor = 'rgb(204, 62, 62)';
+        document.querySelector('.mensagemValidacao').style.display = 'block'
+        resultado.textContent = mensagemError
+        }
+})
+
